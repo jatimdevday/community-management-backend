22b820ceda1725f85c7c7a34f8eff9188febdbc6
Dani Devito <otivedani@outlook.com>
2020-11-03 08:25:17 +0700

    update model:event
    added
    - start and end datetime
    - published, draft status

--- jdd_community_backend/event/models.py
+++ jdd_community_backend/event/models.py
@@ -1,15 +1,54 @@
 from django.db import models
+from datetime import MAXYEAR, datetime, timedelta
+from django.utils.timezone import make_aware, get_default_timezone, now
+from django.core.exceptions import ValidationError
+
+
+class EventQuerySet(models.QuerySet):
+    # published_at < now
+    def published(self):
+        return self.filter(published_at__lt=now())
+
+    # NOT published_at < now
+    def draft(self):
+        return self.exclude(published_at__lt=now())
+
+
+class EventManager(models.Manager):
+    def get_queryset(self):
+        return EventQuerySet(self.model, using=self._db)
+
+    def published(self):
+        return self.get_queryset().published()
+
+    def draft(self):
+        return self.get_queryset().draft()
 
 
 class Event(models.Model):
-    published = models.BooleanField(default=False)
-    title = models.CharField(max_length=200)
+    # model properties 
+    title = models.CharField(max_length=200, blank=False)
     description = models.TextField()
-    start_date = models.DateTimeField()
-    # community_organizer = #ASK use on this model or use 1to1?
+    start_at = models.DateTimeField(blank=False)
+    end_at = models.DateTimeField(blank=False)
+    created_at = models.DateTimeField(auto_now=True)
+    published_at = models.DateTimeField(default=datetime(MAXYEAR, 12, 31, 23, 59, 59, 999999, tzinfo=get_default_timezone())) # set default as 'drafts'
+    # community_organizer #TBA
+    # image_url #TBA
 
+    # model meta
     class Meta:
-        ordering = ['start_date', 'published']
+        ordering = ['start_at', 'published_at']
+
+    # model manager
+    objects = EventManager()
+
+    # override save method
+    def save(self, *args, **kwargs):
+        if self.end_at < self.start_at:
+            raise ValidationError("End date happen before start date.")
+
+        super().save(*args, **kwargs) # actual save
 
     def __str__(self):
         return self.title
--- jdd_community_backend/event/tests.py
+++ jdd_community_backend/event/tests.py
@@ -1,53 +1,102 @@
 from django.test import TestCase
 from event.models import Event
-from datetime import datetime
+from datetime import datetime, MAXYEAR, timedelta
+from django.utils.timezone import make_aware, get_default_timezone
+from django.core.exceptions import ValidationError
 import random
 
 class EventTestCase(TestCase):
-    def setUp(self):
-        pass
-    
+
     """
-    case 1 : create simple event
-    containing title, desc and date.
+    case : create event
+    containing title, desc, date,
     """
     def test_event_creation(self):
         sample_input = {
             'title': 'Event Perdana #1 JDD',
             'description': 'event <i>percobaan</i>',
-            'start_date': datetime(2020,10,30),
+            'start_at': datetime(2020,10,30), # will raise warning since using naive datetime
+            'end_at': datetime(2020,10,30) + timedelta(hours=1), # will raise warning since using naive datetime
         }
         sample_event = Event.objects.create(**sample_input)
 
+        # test : str(model)
         self.assertEqual(str(sample_event), sample_input['title'])
-        self.assertEqual(sample_event.start_date, datetime.strptime('30 October 2020', '%d %B %Y'))
+        # test : date saved
+        self.assertEqual(sample_event.start_at, datetime.strptime('30 October 2020', '%d %B %Y'))
 
 
     """
-    case 2 : create many event
-    containing title, desc and date.
+    case : create many event
+    containing title, desc, date,
     then query.
     """
     def test_event_ordering(self):
-        def randsample_event():
-            communities = ['LinuxBunulrejo', 'PolehanDev', 'NgemplakFrontEnd']
+        def mock_event():
+            mock_title = '#{} Meetup {}'.format(random.randrange(1,99), random.choice(['LinuxBunulrejo', 'PolehanDev', 'NgemplakFrontEnd']))
+            mock_date = make_aware(datetime(2020,10,random.randrange(1,30)))
             yield Event.objects.create(**{
-                'title': '#{} Meetup {}'.format(random.randrange(1,99), random.choice(communities)),
+                'title': mock_title,
                 'description': '',
-                'start_date': datetime(2020,10,random.randrange(1,30)),
+                'start_at': mock_date,
+                'end_at': mock_date + timedelta(hours=1),
             })
 
         # create 20 entry in database...
-        sample_events = [next(randsample_event()) for _ in range(20)]
+        sample_events = [next(mock_event()) for _ in range(20)]
         # ... then sort sample 
-        sample_events.sort(key=lambda k: k.start_date)
+        sample_events.sort(key=lambda k: k.start_at)
 
         # query database
         db_events = Event.objects.all()
 
-        #TODO : sample event was not timezone adjusted (+17)
+        # test : check if each created model and database entry match
         for a, b in zip(db_events, sample_events):
-            self.assertEqual(a.start_date.timestamp(), b.start_date.timestamp())
+            self.assertEqual(a.start_at.timestamp(), b.start_at.timestamp())
             self.assertEqual(a.title, b.title)
             # print('{} = \n{}'.format(a.title, b.title))
+
+
+    """
+    case : create event with invalid date range
+    containing title, desc, date,
+    """
+    def test_event_valid_date(self):
+        sample_input = {
+            'title': 'Event Perdana #1 JDD',
+            'description': 'event <i>percobaan</i>',
+            'start_at': make_aware(datetime.strptime('30 October 2020, 18:00', '%d %B %Y, %H:%M')), 
+            'end_at': make_aware(datetime.strptime('30 October 2020, 17:00', '%d %B %Y, %H:%M')), 
+        }
+        
+        # test : error when end_date is before start_date
+        with self.assertRaises(ValidationError):
+            Event.objects.create(**sample_input)
+
+
+    """
+    case : create event
+    containing title, desc, date, published date,
+    """
+    def test_event_status(self):
+        sample_input = {
+            'title': 'Event Perdana #1 JDD',
+            'description': 'event <i>percobaan</i>',
+            'start_at': make_aware(datetime.strptime('30 October 2020, 18:00', '%d %B %Y, %H:%M')), 
+            'end_at': make_aware(datetime.strptime('30 October 2020, 19:00', '%d %B %Y, %H:%M')), 
+            'published_at': make_aware(datetime.now())
+        }
+        sample_event = Event.objects.create(**sample_input)
+
+        published_events = Event.objects.published()
+        # test : event above is 'published'
+        [self.assertGreaterEqual(make_aware(datetime.now()), ev.published_at) for ev in published_events]
+        
+
+        sample_input.pop('published_at')
+        sample_event = Event.objects.create(**sample_input)
+
+        draft_events = Event.objects.draft()
+        # test : event above is 'draft'
+        [self.assertLessEqual(make_aware(datetime.now()), ev.published_at) for ev in draft_events]
         
